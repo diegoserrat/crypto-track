945687244fb1809b6cd2b195e1efd723be4f384e
Diego Serrat <31071378+diegoserrat@users.noreply.github.com>
2025-07-22 17:10:33 -0300

    creating the service and theirs typo for each

---
+++ src/app/shared/types/crypto.ts
@@ -0,0 +1,31 @@
+export interface Crypto {
+    id: string;
+    symbol: string;
+    name: string;
+    nameid: string;
+    rank: number;
+    price_usd: string;
+    percent_change_24h: string;
+    percent_change_1h: string;
+    percent_change_7d: string;
+    price_btc: string;
+    market_cap_usd: string;
+    volume_24h: number;
+    volume_24a: number;
+    csupply: string;
+    tsupply: string;
+    msupply: string;
+}
+
+export interface GlobalStats {
+    coins_count: number;
+    active_markets: number;
+    total_volume: number;
+    btc_d: string;
+    eth_d: string;
+    mcap_change: string;
+    volume_change: string;
+    avg_change_percent: string;
+    volume_ath: number;
+    mcap_ath: number;
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    coinloreBaseUrl: 'https://api.coinlore.net/api'
+};

b379ac0e8e7320bdbf163a1627e0e20e282ffb61
Diego Serrat <31071378+diegoserrat@users.noreply.github.com>
2025-07-22 18:29:25 -0300

    creating list component

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'crypto-track-list',
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+  
+})
+export class ListComponent {
+}
--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from '../../components/list/list.component';
 
 @Component({
   selector: 'crypto-home',
-  imports: [],
+  imports: [CommonModule, ListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
